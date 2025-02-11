1cee3e788fb058c13cf85e276f170cd7227d510e
yunus kupucu <yunus.kupucu@gmail.com>
2025-02-11 19:46:24 +0300

    register page uı completed

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,128 @@
+import { useState } from 'react';
+import {
+  Container,
+  Button,
+  Typography,
+  Paper,
+  Box,
+  Link,
+  Input,
+} from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
+function Register() {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [fullname, setFullname] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Giriş işlemi burada yapılacak
+    console.log('Email:', email, 'Password:', password);
+  };
+
+  return (
+    <Container
+      maxWidth="xs"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+      }}
+    >
+      <Paper
+        elevation={3}
+        sx={{
+          padding: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          borderRadius: 3,
+          width: '400px',
+        }}
+      >
+        <Typography
+          variant="h4"
+          component="h1"
+          gutterBottom
+          sx={{
+            mb: 5,
+            fontFamily: '"Newsreader" ,serif',
+            fontWeight: '500',
+            fontSize: '40px',
+          }}
+        >
+          SEKTÖR
+        </Typography>
+
+        <Box component="form" sx={{ width: '100%' }} onSubmit={handleSubmit}>
+          <label style={{ fontSize: '24px' }}>Ad Soyad</label>
+          <Input
+            fullWidth
+            margin="normal"
+            value={fullname}
+            onChange={(e) => setFullname(e.target.value)}
+            required
+            placeholder="ad soyad"
+            sx={{ mb: 5, mt: 2 }}
+          />
+          <label style={{ fontSize: '24px' }}>E-POSTA</label>
+          <Input
+            fullWidth
+            margin="normal"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            placeholder="you@example.com"
+            sx={{ mb: 5, mt: 2 }}
+          />
+          <label style={{ fontSize: '24px' }}>PAROLA</label>
+          <Input
+            fullWidth
+            margin="normal"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            placeholder="******"
+            type="password"
+            sx={{ mb: 2, mt: 2 }}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{
+              mt: 2,
+              backgroundColor: '#00a1ff',
+              fontSize: '18px',
+              padding: '10px 0',
+              width: '100%',
+              borderRadius: '5px',
+            }}
+          >
+            Kaydol
+          </Button>
+        </Box>
+
+        <Typography variant="body1" sx={{ mt: 2 }}>
+          Hesabınız var mı?
+          <Link
+            onClick={() => {
+              navigate('/login');
+            }}
+            component="button"
+            sx={{ ml: 1, color: '#1495e0', fontWeight: 'bold' }}
+          >
+            Giriş Yap
+          </Link>
+        </Typography>
+      </Paper>
+    </Container>
+  );
+}
+
+export default Register;
