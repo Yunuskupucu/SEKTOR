08d416fa766108b136d23d9b782a8d6c159b51a5
yunus kupucu <yunus.kupucu@gmail.com>
2025-02-12 00:08:35 +0300

    add profile route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Login from './pages/Login';
 import './index.scss';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
   );
