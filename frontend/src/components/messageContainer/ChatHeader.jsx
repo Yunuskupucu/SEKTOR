094743fdb9938a80752cbe6bb92ca0f64a568869
yunus kupucu <yunus.kupucu@gmail.com>
2025-02-14 17:34:34 +0300

    Chat Header Added

---
+++ frontend/src/components/messageContainer/ChatHeader.jsx
@@ -0,0 +1,11 @@
+import '../../styles/MessageContainer.scss';
+
+const ChatHeader = () => {
+  return (
+    <div className="chatHeader">
+      <h1>Channel Name</h1>
+    </div>
+  );
+};
+
+export default ChatHeader;
