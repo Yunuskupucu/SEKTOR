460f7aa9ffdff452252b872967b79d8093a54bbb
yunus kupucu <yunus.kupucu@gmail.com>
2025-03-04 01:52:55 +0300

    Refactor generateToken utility function with code formatting

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,17 +1,17 @@
 // ? mevcut kod bloğunu yeniden kullanabileceğimiz genel amaçlı bir yardımcı sınıf
 import jwt from 'jsonwebtoken';
-export const generateToken= (userId,res) => {
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-        expiresIn: "7d",  // 7 gün geçerli token
-})
-// Cookie'ye token'ı yazma
-res.cookie("jwt", token, {  
- maxAge: 1000 * 60 * 60 * 24 * 7, // 7 gün
- httpOnly: true, //Cookie, sadece HTTP istekleriyle erişilebilir, JavaScript ile erişilemez.
- sameSite : "strict", // Cookie, sadece HTTPS üzerinden gönderilir (geliştirme ortamı dışında)
- secure : process.env.NODE_ENV !== "development"
-})
+export const generateToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: '7d', // 7 gün geçerli token
+  });
+  // Cookie'ye token'ı yazma
+  res.cookie('jwt', token, {
+    maxAge: 1000 * 60 * 60 * 24 * 7, // 7 gün
+    httpOnly: true, //Cookie, sadece HTTP istekleriyle erişilebilir, JavaScript ile erişilemez.
+    sameSite: 'strict', // Cookie, sadece HTTPS üzerinden gönderilir (geliştirme ortamı dışında)
+    secure: process.env.NODE_ENV !== 'development',
+  });
 
-return  token;
+  return token;
 };
 // ? mevcut kod bloğunu yeniden kullanabileceğimiz genel amaçlı bir yardımcı sınıf
