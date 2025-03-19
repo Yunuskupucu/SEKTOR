1bad7539e9d28e7d5dbfd0418f53c85995cf93fd
yunus kupucu <yunus.kupucu@gmail.com>
2025-03-19 13:07:36 +0300

    Add theme toggle functionality to Header component and update styles for dark mode support

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,16 @@
-import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FiSettings, FiUser, FiLogOut } from 'react-icons/fi';
 import styles from '../styles/Header.module.scss';
 import { useAuthStore } from '../store/useAuthStore';
+import { useTheme } from '../context/useTheme';
+import { CgDarkMode } from 'react-icons/cg';
 
 function Header() {
   const [anchorEl, setAnchorEl] = useState(false);
   const navigate = useNavigate();
   const { logout } = useAuthStore();
+  const { theme, toggleTheme } = useTheme();
 
   const handleMenuOpen = () => {
     setAnchorEl(!anchorEl);
@@ -33,27 +36,35 @@ function Header() {
       handleMenuClose();
     }
   };
+  console.log('Theme:', theme);
 
   return (
     <header className={styles.header}>
-      <button className={styles.logo} onClick={() => navigate('/home')}>
+      <button className={styles.logo} onClick={() => navigate('/')}>
         <span>S</span>
         <label>SEKTÖR</label>
       </button>
 
-      <div className={styles.settings}>
-        <div className={styles.settingsIcon} onClick={handleMenuOpen}>
-          <FiSettings size={24} />
+      <div className={styles.settingsContainer}>
+        <div>
+          <CgDarkMode className={styles.themeIcon} onClick={toggleTheme} />
         </div>
-
-        <div className={`${styles.dropdown} ${anchorEl ? styles.active : ''}`}>
-          <div className={styles.menuItem} onClick={handleProfile}>
-            <FiUser size={18} />
-            Profil
+        <div className={styles.settings}>
+          <div className={styles.settingsIcon} onClick={handleMenuOpen}>
+            <FiSettings size={24} />
           </div>
-          <div className={styles.menuItem} onClick={handleLogout}>
-            <FiLogOut size={18} />
-            Çıkış
+
+          <div
+            className={`${styles.dropdown} ${anchorEl ? styles.active : ''}`}
+          >
+            <div className={styles.menuItem} onClick={handleProfile}>
+              <FiUser size={18} />
+              Profil
+            </div>
+            <div className={styles.menuItem} onClick={handleLogout}>
+              <FiLogOut size={18} />
+              Çıkış
+            </div>
           </div>
         </div>
       </div>
