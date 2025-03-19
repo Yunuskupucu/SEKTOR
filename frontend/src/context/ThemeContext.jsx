7cae16304a5dee283c764b15d6207fa3e6231261
yunus kupucu <yunus.kupucu@gmail.com>
2025-03-19 12:53:19 +0300

    Add ThemeContext and useTheme hook for theme management

---
+++ frontend/src/context/ThemeContext.jsx
@@ -0,0 +1,28 @@
+import { createContext, useState } from 'react';
+import PropTypes from 'prop-types';
+
+const ThemeContext = createContext();
+
+function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState('');
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    if (newTheme === 'dark') {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  };
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+ThemeProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export { ThemeProvider, ThemeContext }; // ThemeProvider'ı named export olarak dışa aktar
