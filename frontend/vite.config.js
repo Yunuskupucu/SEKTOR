e05070b4af1d4fbabae6b5de380118720ecf672a
Yunuskupucu <yunus.kupucu@gmail.com>
2026-05-03 01:51:22 +0300

    ...

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
+
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
